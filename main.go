65d4f779718c9ead17a3568da31905d63fdfaf54
StanawalkaWorld <stanawalkaworld@gmail.com>
2025-08-07 23:06:14 +0200

    Basic CSV reading

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	file, err := os.Open("test.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+
+	for {
+		line, err := csvReader.Read()
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			log.Println(err)
+			continue
+		}
+
+		for i, element := range line {
+			if i < len(line)-1 {
+				fmt.Printf("%s - ", element)
+			} else {
+				fmt.Print(element)
+			}
+		}
+		fmt.Println()
+	}
+
 }
